3b71ed121b280fae9246a2ad116e14dcf03edf11
Abdelrahman Rafaat <abdelrahman.raafaat@gmail.com>
2023-07-13 00:37:57 +0200

    Add splash Screen

---
+++ app/src/main/java/com/abdelrahman/raafaat/monkeybanana/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.abdelrahman.raafaat.monkeybanana
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(2000)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        }
+    }
+}
